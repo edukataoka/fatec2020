6f290b0fd2af22e6329cd3d0ca9b8fa0c2d28bc3
edukataoka <47614393+edukataoka@users.noreply.github.com>
2020-05-22 22:34:49 -0300

    Add files via upload

---
+++ les/aulaonline01/MeuServlet.java
@@ -0,0 +1,45 @@
+package dudu;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet("/meuservlet")
+
+	public class MeuServlet implements Servlet {
+		  private ServletConfig config;
+		  
+			@Override
+			public void destroy() {
+				
+			}
+
+			@Override
+			public ServletConfig getServletConfig() {
+				return config;
+				
+			}
+
+			@Override
+			public String getServletInfo() {
+				return "Servlet: " + MeuServlet.class.getName();
+			}
+
+			@Override
+			public void init(ServletConfig cfg) throws ServletException {
+				this.config = cfg;
+				
+			}
+
+			@Override
+			public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+				System.out.println("Servlet acionado");
+				System.out.println(new Date());
+				
+}}
