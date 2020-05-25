b95e62ddf4c9cdb2c4c6d91509b448b1b149df40
edukataoka <47614393+edukataoka@users.noreply.github.com>
2020-05-25 19:26:57 -0300

    Add files via upload

---
+++ les/aulaonline02/MeuHttpServlet.java
@@ -0,0 +1,34 @@
+package edu.curso;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servico")
+public class MeuHttpServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		this.processa(req, res);
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		this.processa(req, res);
+	}
+
+	public void processa(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out = res.getWriter();
+		out.println("<h1>Você acessou este servlet via GET</h1>");
+
+
+		String ua = req.getHeader("user-agent");
+		out.println("Você está acessando de um: " + ua);
+	}
+
+}
